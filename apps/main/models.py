4c54f9d5a2314afe700e88119cdd585c2ae86f94
overnote <overmes@gmail.com>
2013-09-07 23:41:43 +0400

    form

--- apps/main/models.py
+++ apps/main/models.py
@@ -4,14 +4,14 @@ from picklefield.fields import PickledObjectField
 
 class Place(models.Model):
     place_id = models.CharField(max_length=500)
-    # data = PickledObjectField()
-    # day1 = PickledObjectField()
-    # day2 = PickledObjectField()
-    # day3 = PickledObjectField()
-    # day4 = PickledObjectField()
-    # day5 = PickledObjectField()
-    # day6 = PickledObjectField()
-    # day7 = PickledObjectField()
+    data = PickledObjectField()
+    day1 = PickledObjectField()
+    day2 = PickledObjectField()
+    day3 = PickledObjectField()
+    day4 = PickledObjectField()
+    day5 = PickledObjectField()
+    day6 = PickledObjectField()
+    day7 = PickledObjectField()
 
 
 class InstagramImage(models.Model):
