78102541d100c46c397eec47cec0224f2ba2c8b6
Vitaliy Korobkin <vitalisbox@gmail.com>
2013-09-07 18:18:44 +0400

    places

--- apps/main/placer.py
+++ apps/main/placer.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import foursquare
+from apps.main.models import Place
 
 client = foursquare.Foursquare(
     client_id='BJ3NANVVRNF5RIQCPVTORRCV13UHCPAVYK1EF5S43OK0MAA2',
@@ -7,6 +8,22 @@ client = foursquare.Foursquare(
 )
 
 
+def places():
+    result = []
+    places = Place.objects.all()
+    for p in places:
+        result.append(p.data)
+    return result
+
+
 def get_places(categoryId):
     places = client.venues.search(params={'categoryId': categoryId, 'intent': 'browse', 'near': 'Moscow', 'limit': 50})
     return places['venues']
+
+
+def update(categoryId):
+    places = placer.get_places('4bf58dd8d48988d163941735')  # Парки
+    for place in places:
+        Place.objects.get_or_create(place_id=place['id'], defaults={
+            'data': place
+        })
