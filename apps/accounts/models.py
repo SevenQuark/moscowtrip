9a492cf7a4aba095301b3eaddfaa1b3656069057
overnote <overmes@gmail.com>
2013-09-07 15:49:32 +0400

    dashboard model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,7 @@
+import datetime
 from django.utils.translation import ugettext as _
 from auth_pack.models import AbstractEmailUser
+from django.db import models
 
 
 class User(AbstractEmailUser):
@@ -7,3 +9,9 @@ class User(AbstractEmailUser):
     class Meta:
         verbose_name = _('User')
         verbose_name_plural = _('Users')
+
+
+class DashboardModel(models.Model):
+    date_from = models.DateTimeField(default=lambda: datetime.datetime.now() - datetime.timedelta(days=-3))
+    date_to = models.DateTimeField(default=datetime.datetime.now)
+    location_from = models.CharField(max_length=255)
