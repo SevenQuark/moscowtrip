6a46e65ebd3eff7c4a620106520e5d295fc19d26
madAsket <madasket@gmail.com>
2013-09-08 12:14:34 +0400

    complete page

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -2,6 +2,7 @@ from apps.main.views import PlainTextTemplateView, PayPal, DashboardCreateView
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from apps.main.views import Places
+from django.views.generic import TemplateView
 
 
 admin.autodiscover()
@@ -12,5 +13,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^places/', Places.as_view()),
     url(r'^paypal/', PayPal.as_view()),
-    (r'^dashboard/', include('apps.accounts.urls', namespace = 'dashboard'))
+    (r'^dashboard/', include('apps.accounts.urls', namespace = 'dashboard')),
+    (r'^complete/', TemplateView.as_view(template_name='purchase/complete.html'))
 )
