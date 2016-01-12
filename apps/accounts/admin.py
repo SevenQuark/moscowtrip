25316c4d33354c91710e3a5b820e86800c3f09ad
Denis Volkov <madAsket@gmail.com>
2016-01-12 18:53:53 +0300

    admin is updated

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -3,6 +3,5 @@ from apps.accounts.models import User
 from auth_pack.admin import EmailUserAdmin
 from django.contrib.auth.models import Group
 
-
 admin.site.register(User, EmailUserAdmin)
 admin.site.unregister(Group)
